259da71df17260786a8f83fa423a833c2cb0ea84
Benno van den Berg <bennovandenberg@gmail.com>
2015-05-28 15:21:10 +0200

    Update xmonad.hs

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -3,18 +3,18 @@ import System.IO
 import XMonad
 import XMonad.Core
 import XMonad.Hooks.EwmhDesktops
-import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Util.EZConfig
 
 myKeys =
   [
-    ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
-    , ((0                     , xF86XK_AudioPlay), spawn "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause")
-    , ((0                     , xF86XK_AudioLowerVolume), spawn "amixer set Master 3-")
-    , ((0                     , xF86XK_AudioRaiseVolume), spawn "amixer set Master 3+")
-    , ((0                     , xF86XK_AudioMute), spawn "amixer set Master toggle")
+    (("M4-l"), spawn "slock"),
+    (("<XF86AudioPlay>"), spawn "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause"),
+    (("<XF86AudioLowerVolume>"), spawn "amixer set Master 3-"),
+    (("<XF86AudioRaiseVolume>"), spawn "amixer set Master 3+"),
+    (("<XF86AudioMute>"), spawn "amixer set Master toggle")
   ]
 
 main = xmonad $ ewmh defaultConfig{
     handleEventHook = handleEventHook defaultConfig <+> fullscreenEventHook
   }
-  `additionalKeys` myKeys
+  `additionalKeysP` myKeys
