5b91802c6ceea8f8dbc7c7d4c9476a05b7bb0a0b
Benno van den Berg <bennovandenberg@gmail.com>
2015-05-28 15:55:37 +0200

    Update xmonad.hs

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -9,6 +9,8 @@ myKeys =
   [
     (("M4-l"), spawn "slock"),
     (("<XF86AudioPlay>"), spawn "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause"),
+    (("<XF86Back>"), spawn "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Previous"),
+    (("<XF86Forward>"), spawn "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Next"),
     (("<XF86AudioLowerVolume>"), spawn "amixer set Master 3-"),
     (("<XF86AudioRaiseVolume>"), spawn "amixer set Master 3+"),
     (("<XF86AudioMute>"), spawn "amixer set Master toggle")
