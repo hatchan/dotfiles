71680f7cf5485c6b4d52f50f1063d781bc1b2861
Benno van den Berg <hatchan@users.noreply.github.com>
2017-04-18 15:30:54 +0200

    Update xmonad.hs

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -18,7 +18,9 @@ myKeys =
     (("<XF86XF86Forward>"), spawn "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Next"),
     (("<XF86AudioLowerVolume>"), spawn "amixer set Master 3-"),
     (("<XF86AudioRaiseVolume>"), spawn "amixer set Master 3+"),
-    (("<XF86AudioMute>"), spawn "amixer -D pulse set Master toggle")
+    (("<XF86AudioMute>"), spawn "amixer -D pulse set Master toggle"),
+    (("<XF86MonBrightnessUp>"), spawn "xbacklight -inc 10 -time 0"),
+    (("<XF86MonBrightnessDown>"), spawn "xbacklight -dec 10 -time 0")
   ]
 
 --xmproc <- spawnPipe "/path/to/xmobarbinary /home/jgoerzen/.xmobarrc"
