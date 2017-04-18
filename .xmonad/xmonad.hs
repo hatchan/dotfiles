ee1c48fc14fc171ea785f0d390bd251c113fa0af
Benno van den Berg <hatchan@users.noreply.github.com>
2017-04-18 15:25:11 +0200

    Update xmonad.hs

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -12,17 +12,19 @@ myKeys =
   [
     (("M4-l"), spawn "slock"),
     (("<XF86AudioPlay>"), spawn "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause"),
+    (("<XF86AudioPrev>"), spawn "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Previous"),
     (("<XF86Back>"), spawn "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Previous"),
-    (("<XF86Forward>"), spawn "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Next"),
+    (("<XF86AudioNext>"), spawn "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Next"),
+    (("<XF86XF86Forward>"), spawn "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Next"),
     (("<XF86AudioLowerVolume>"), spawn "amixer set Master 3-"),
     (("<XF86AudioRaiseVolume>"), spawn "amixer set Master 3+"),
-    (("<XF86AudioMute>"), spawn "amixer set Master toggle")
+    (("<XF86AudioMute>"), spawn "amixer -D pulse set Master toggle")
   ]
 
 --xmproc <- spawnPipe "/path/to/xmobarbinary /home/jgoerzen/.xmobarrc"
 
 main = do
-  xmproc <- spawnPipe "/usr/bin/xmobar"
+  xmproc <- spawnPipe "/home/hatchan/bin/xmobar"
   xmonad $ ewmh defaultConfig
     { startupHook = spawn "xset r rate 300 30"
     , manageHook = manageDocks <+> manageHook defaultConfig
