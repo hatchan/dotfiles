676aa801ea3d39780e009a629aeadeb3915c65dc
Benno van den Berg <hatchan@users.noreply.github.com>
2020-11-18 12:01:15 +0100

    Update long_process.sh

--- .zsh_scripts/long_process.sh
+++ .zsh_scripts/long_process.sh
@@ -70,11 +70,11 @@ zbell_end() {
 		if (( exit_code == 0)); then
 			msg="Command finished (${duration}s)"
 			icon="terminal"
-      timeout=5000
+			timeout=5000
 		else
 			msg="Command failed: $exit_code (${duration}s)"
 			icon="error"
-      timeout=10000
+			timeout=10000
 		fi
 		notify-send -i "$icon" -t "$timeout" "$msg" "$zbell_lastcmd"
 	fi
