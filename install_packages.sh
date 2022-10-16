546504079c3fcd0bf77ab248936899c859b22eb1
Benno van den Berg <bennovandenberg@gmail.com>
2022-10-16 20:27:57 +0200

    Add install_packges.sh

---
+++ install_packages.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+yay -S --needed \
+	extra/vim \
+	extra/zsh \
+	aur/nerd-fonts-fira-code \
+	community/starship \
+	extra/gnome-keyring \
+	aur/visual-studio-code-bin \
+	aur/google-chrome \
+	community/intel-media-driver \
+	extra/vlc \
+	aur/spotify \
+	community/playerctl \
+	community/pamixer \
+	aur/slack-desktop \
+	community/fwupd \
+	community/the_silver_searcher \
+	community/direnv \
+	community/podman \
+	community/alacritty \
+	aur/pcloud-drive \
+	community/sccache \
+	community/mold \
+	extra/clang
