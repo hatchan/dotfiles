0594fc5bf172e580ec253be2227c14970e2fa515
Benno van den Berg <bennovandenberg@gmail.com>
2023-07-28 17:54:46 +0200

    Cleanup and more updates

--- .config/zsh/.zshrc
+++ .config/zsh/.zshrc
@@ -10,21 +10,16 @@ if [[ -s "${ZDOTDIR:-$HOME}/.zprezto/init.zsh" ]]; then
   source "${ZDOTDIR:-$HOME}/.zprezto/init.zsh"
 fi
 
+# Customize to your needs...
+
 export PATH="$HOME/.local/bin:$PATH"
 
-# Customize to your needs...
 if whence starship >& /dev/null; then
   eval "$(starship init zsh)"
 fi
 
 if whence direnv >& /dev/null; then
   eval "$(direnv hook zsh)"
-fi
-
-if [ -e "/usr/share/nvm/nvm.sh" ] ; then
-  export NVM_DIR="$HOME/.nvm"
-  source /usr/share/nvm/nvm.sh
-  # Remember to run /usr/share/nvm/install-nvm-exec once
 fi
 
 if [[ -s "/usr/share/fzf/key-bindings.zsh" ]]; then
--- install_packages.sh
+++ install_packages.sh
@@ -5,47 +5,49 @@ IFS=$'\n\t'
 yay -S --needed \
 	extra/vim \
 	extra/zsh \
-	community/ttf-firacode-nerd \
-	community/starship \
+	extra/ttf-firacode-nerd \
+	extra/starship \
 	extra/gnome-keyring \
 	aur/visual-studio-code-bin \
 	aur/google-chrome \
-	community/intel-media-driver \
+	extra/intel-media-driver \
 	extra/vlc \
 	aur/spotify \
-	community/playerctl \
-	community/pamixer \
+	extra/playerctl \
+	extra/pamixer \
 	aur/slack-desktop \
-	community/fwupd \
-	community/the_silver_searcher \
-	community/direnv \
-	community/podman \
-	community/alacritty \
+	extra/fwupd \
+	extra/the_silver_searcher \
+	extra/direnv \
+	extra/podman \
+	extra/alacritty \
 	aur/pcloud-drive \
-	community/sccache \
-	community/mold \
+	extra/sccache \
+	extra/mold \
 	extra/clang \
-	community/thermald \
-	community/github-cli \
-	community/aws-cli-v2 \
-	community/kubectl \
-	community/kubectx \
+	extra/thermald \
+	extra/github-cli \
+	extra/aws-cli-v2 \
+	extra/kubectl \
+	extra/kubectx \
 	aur/tilt-bin \
 	aur/ctlptl-bin \
 	aur/kind-bin \
-	community/terraform \
-	community/cargo-nextest \
-	community/helm \
-	community/discord \
+	extra/terraform \
+	extra/cargo-nextest \
+	extra/helm \
+	extra/discord \
 	aur/ookla-speedtest-bin \
 	aur/protonvpn \
-	community/yt-dlp \
+	extra/yt-dlp \
 	extra/smbclient \
-	community/obsidian \
-	community/arduino-cli \
+	extra/obsidian \
+	extra/arduino-cli \
 	aur/arduino-ide-bin \
 	aur/phoronix-test-suite \
-	community/binaryen \
-	community/visidata \
-	community/fzf \
-	wireguard-tools
+	extra/binaryen \
+	extra/visidata \
+	extra/fzf \
+	wireguard-tools \
+	aur/1password-cli \
+	aur/1password
